6951b4c0709917ee1a2956bc49a4c60cc748b208
Artur Gubaydulin <megaarcher@mail.ru>
2024-06-13 12:57:19 +0500

    making first array

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите размер текстового массива:");
+int size = int.Parse(Console.ReadLine()!);
+String[] arr1 = new String[size];
+
+Console.WriteLine($"Введите {size} элементов текстового массива:");
+for (int i = 0; i < size; i++)
+{
+    arr1[i] = Console.ReadLine();
+}
+
