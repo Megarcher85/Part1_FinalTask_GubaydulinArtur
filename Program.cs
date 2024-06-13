194ff2d57c445a9cff307a47e88e5b067ed3f20d
Artur Gubaydulin <megaarcher@mail.ru>
2024-06-13 13:00:41 +0500

    making second array

--- Program.cs
+++ Program.cs
@@ -17,3 +17,15 @@ for (int i = 0; i < arr1.Length; i++)
     };
 }
 
+String[] arr2 = new String[size2];
+
+int j = 0;
+for (int i = 0; i < arr1.Length; i++)
+{
+    if (arr1[i].Length <= 3)
+    {
+        arr2[j] = arr1[i];
+        j++;
+    };
+}
+
