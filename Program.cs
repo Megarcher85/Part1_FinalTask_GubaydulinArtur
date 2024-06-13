e5e5dc2e363e984cfa4bc09142a46d7f96eec179
Artur Gubaydulin <megaarcher@mail.ru>
2024-06-13 12:59:19 +0500

    getting size for second array

--- Program.cs
+++ Program.cs
@@ -8,3 +8,12 @@ for (int i = 0; i < size; i++)
     arr1[i] = Console.ReadLine();
 }
 
+int size2 = 0;
+for (int i = 0; i < arr1.Length; i++)
+{
+    if (arr1[i].Length <= 3)
+    {
+        size2 += 1;
+    };
+}
+
